palatable/lambda

Implement TakeWhileM
Implement a TakeWhileM Fn2 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/TakeWhileM.java
@@ -1,0 +1,46 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.iterateT;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
+
+/**
+ * Lazily limit the <code>IterateT</code> to the first group of contiguous elements that satisfy the predicate by
+ * iterating up to, but not including, the first element for which the predicate evaluates to <code>false</code>.
+ *
+ * @param <A> The IterateT element type
+ * @param <M> the IterateT effect type
+ * @see TakeM
+ * @see FilterM
+ * @see DropWhileM
+ */
+public class TakeWhileM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final TakeWhileM<?, ?> INSTANCE = new TakeWhileM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return iterateT(maybeT(as.runIterateT())
+                                .filter(predicate.diMapL(Tuple2::_1))
+                                .fmap(t -> t.fmap(takeWhileM(predicate)))
+                                .runMaybeT());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> TakeWhileM<A, M> takeWhileM() {
+        return (TakeWhileM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> takeWhileM(Fn1<? super A, ? extends Boolean> predicate) {
+        return TakeWhileM.<A, M>takeWhileM().apply(predicate);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> takeWhileM(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return TakeWhileM.<A, M>takeWhileM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/TakeWhileMTest.java
@@ -1,0 +1,66 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GT.gt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LTE.lte;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeWhileM.takeWhileM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.semigroup.builtin.Max.max;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class TakeWhileMTest {
+
+    public static final Fn1<Integer, Integer> MOD_10 = i -> i % 10;
+
+    @Test
+    public void takeEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(takeWhileM(lt(5), numbers), isEmpty());
+    }
+
+    @Test
+    public void takeNothing() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(gt(5), numbers), isEmpty());
+    }
+
+    @Test
+    public void takeSomeInfinite() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(lte(5), numbers), iterates(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void takeNothingIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(gt(5).diMapL(MOD_10), numbers), isEmpty());
+    }
+
+    @Test
+    public void takeSomeIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(lte(5).diMapL(MOD_10), numbers), iterates(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void takeStackSafe() {
+        IterateT<IO<?>, Integer> numbers = naturalNumbersM(pureIO());
+        assertThat(takeWhileM(lte(STACK_EXPLODING_NUMBER), numbers).fold((a, i) -> io(max(a, i)), io(0)),
+                   yieldsValue(equalTo(STACK_EXPLODING_NUMBER)));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/TakeWhileM.java
@@ -1,0 +1,46 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.iterateT;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
+
+/**
+ * Lazily limit the <code>IterateT</code> to the first group of contiguous elements that satisfy the predicate by
+ * iterating up to, but not including, the first element for which the predicate evaluates to <code>false</code>.
+ *
+ * @param <A> The IterateT element type
+ * @param <M> the IterateT effect type
+ * @see TakeM
+ * @see FilterM
+ * @see DropWhileM
+ */
+public class TakeWhileM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final TakeWhileM<?, ?> INSTANCE = new TakeWhileM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return iterateT(maybeT(as.runIterateT())
+                                .filter(predicate.diMapL(Tuple2::_1))
+                                .fmap(t -> t.fmap(takeWhileM(predicate)))
+                                .runMaybeT());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> TakeWhileM<A, M> takeWhileM() {
+        return (TakeWhileM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> takeWhileM(Fn1<? super A, ? extends Boolean> predicate) {
+        return TakeWhileM.<A, M>takeWhileM().apply(predicate);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> takeWhileM(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return TakeWhileM.<A, M>takeWhileM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/TakeWhileMTest.java
@@ -1,0 +1,66 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GT.gt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LTE.lte;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeWhileM.takeWhileM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.semigroup.builtin.Max.max;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class TakeWhileMTest {
+
+    public static final Fn1<Integer, Integer> MOD_10 = i -> i % 10;
+
+    @Test
+    public void takeEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(takeWhileM(lt(5), numbers), isEmpty());
+    }
+
+    @Test
+    public void takeNothing() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(gt(5), numbers), isEmpty());
+    }
+
+    @Test
+    public void takeSomeInfinite() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(lte(5), numbers), iterates(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void takeNothingIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(gt(5).diMapL(MOD_10), numbers), isEmpty());
+    }
+
+    @Test
+    public void takeSomeIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeWhileM(lte(5).diMapL(MOD_10), numbers), iterates(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void takeStackSafe() {
+        IterateT<IO<?>, Integer> numbers = naturalNumbersM(pureIO());
+        assertThat(takeWhileM(lte(STACK_EXPLODING_NUMBER), numbers).fold((a, i) -> io(max(a, i)), io(0)),
+                   yieldsValue(equalTo(STACK_EXPLODING_NUMBER)));
+    }
+}