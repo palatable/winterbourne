palatable/lambda

Implement RepeatM
Implement a RepeatM Fn1 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn1/RepeatM.java
@@ -1,0 +1,38 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+
+/**
+ * Given a value in monadic effect, return an infinite <code>IterateT</code> that repeatedly iterates that value.
+ *
+ * @param <M> the IterateT effect type
+ * @param <A> The IterateT element type
+ */
+public class RepeatM<M extends MonadRec<?, M>, A> implements Fn1<MonadRec<A, M>, IterateT<M, A>> {
+
+    private static final RepeatM<?, ?> INSTANCE = new RepeatM<>();
+
+    private RepeatM() {
+    }
+
+    @Override
+    public IterateT<M, A> checkedApply(MonadRec<A, M> ma) throws Throwable {
+        return suspended(() -> ma.fmap(a -> just(tuple(a, repeatM(ma)))), Pure.of(ma));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> RepeatM<M, A> repeatM() {
+        return (RepeatM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, A> repeatM(MonadRec<A, M> ma) {
+        return RepeatM.<M, A>repeatM().apply(ma);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn1/RepeatMTest.java
@@ -1,0 +1,28 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.RepeatM.repeatM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.NthM.nthM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class RepeatMTest {
+
+    @Test
+    public void repeatsAThing() {
+        assertThat(takeM(10, repeatM(new Identity<>(1))), iterates(1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+    }
+
+    @Test
+    public void repeatsALotOfThings() {
+        Identity<Maybe<Integer>> actual = nthM(STACK_EXPLODING_NUMBER, repeatM(new Identity<>(1))).coerce();
+        assertThat(actual.runIdentity(), equalTo(just(1)));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/RepeatM.java
@@ -1,0 +1,38 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+
+/**
+ * Given a value in monadic effect, return an infinite <code>IterateT</code> that repeatedly iterates that value.
+ *
+ * @param <M> the IterateT effect type
+ * @param <A> The IterateT element type
+ */
+public class RepeatM<M extends MonadRec<?, M>, A> implements Fn1<MonadRec<A, M>, IterateT<M, A>> {
+
+    private static final RepeatM<?, ?> INSTANCE = new RepeatM<>();
+
+    private RepeatM() {
+    }
+
+    @Override
+    public IterateT<M, A> checkedApply(MonadRec<A, M> ma) throws Throwable {
+        return suspended(() -> ma.fmap(a -> just(tuple(a, repeatM(ma)))), Pure.of(ma));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> RepeatM<M, A> repeatM() {
+        return (RepeatM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, A> repeatM(MonadRec<A, M> ma) {
+        return RepeatM.<M, A>repeatM().apply(ma);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/RepeatMTest.java
@@ -1,0 +1,28 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.RepeatM.repeatM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.NthM.nthM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class RepeatMTest {
+
+    @Test
+    public void repeatsAThing() {
+        assertThat(takeM(10, repeatM(new Identity<>(1))), iterates(1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+    }
+
+    @Test
+    public void repeatsALotOfThings() {
+        Identity<Maybe<Integer>> actual = nthM(STACK_EXPLODING_NUMBER, repeatM(new Identity<>(1))).coerce();
+        assertThat(actual.runIdentity(), equalTo(just(1)));
+    }
+}