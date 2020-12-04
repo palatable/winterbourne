palatable/lambda

Implement MagnetizeM
Implement a MagnetizeM Fn1 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn1/MagnetizeM.java
@@ -1,0 +1,36 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Eq.eq;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.MagnetizeByM.magnetizeByM;
+
+/**
+ * {@link Magnetize} an {@link IterateT} using value equality as the magnetizing function.
+ *
+ * @param <A> the IterateT element type
+ * @param <M> the IterateT effect type
+ */
+public class MagnetizeM<M extends MonadRec<?, M>, A> implements Fn1<IterateT<M, A>, IterateT<M, IterateT<M, A>>> {
+
+    private static final MagnetizeM<?, ?> INSTANCE = new MagnetizeM<>();
+
+    private MagnetizeM() {
+    }
+
+    @Override
+    public IterateT<M, IterateT<M, A>> checkedApply(IterateT<M, A> mas) throws Throwable {
+        return magnetizeByM(eq(), mas);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> MagnetizeM<M, A> magnetizeM() {
+        return (MagnetizeM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, IterateT<M, A>> magnetizeM(IterateT<M, A> mas) {
+        return MagnetizeM.<M, A>magnetizeM().apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn1/MagnetizeMTest.java
@@ -1,0 +1,33 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.MagnetizeM.magnetizeM;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.RepeatM.repeatM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.*;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class MagnetizeMTest {
+
+    @Test
+    public void groupsLikeElements() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()).flatMap(n -> takeM(n, repeatM(new Identity<>(n)))));
+        List<IterateT<Identity<?>, Integer>> actual = magnetizeM(numbers)
+                .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                .runIdentity();
+
+        assertThat(actual, contains(iterates(1),
+                                    iterates(2, 2),
+                                    iterates(3, 3, 3),
+                                    iterates(4, 4, 4, 4)));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/MagnetizeM.java
@@ -1,0 +1,37 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.builtin.fn1.Magnetize;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Eq.eq;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.MagnetizeByM.magnetizeByM;
+
+/**
+ * {@link Magnetize} an {@link IterateT} using value equality as the magnetizing function.
+ *
+ * @param <A> the IterateT element type
+ * @param <M> the IterateT effect type
+ */
+public class MagnetizeM<M extends MonadRec<?, M>, A> implements Fn1<IterateT<M, A>, IterateT<M, IterateT<M, A>>> {
+
+    private static final MagnetizeM<?, ?> INSTANCE = new MagnetizeM<>();
+
+    private MagnetizeM() {
+    }
+
+    @Override
+    public IterateT<M, IterateT<M, A>> checkedApply(IterateT<M, A> mas) throws Throwable {
+        return magnetizeByM(eq(), mas);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> MagnetizeM<M, A> magnetizeM() {
+        return (MagnetizeM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, IterateT<M, A>> magnetizeM(IterateT<M, A> mas) {
+        return MagnetizeM.<M, A>magnetizeM().apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/MagnetizeMTest.java
@@ -1,0 +1,33 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.MagnetizeM.magnetizeM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.RepeatM.repeatM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.*;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class MagnetizeMTest {
+
+    @Test
+    public void groupsLikeElements() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()).flatMap(n -> takeM(n, repeatM(new Identity<>(n)))));
+        List<IterateT<Identity<?>, Integer>> actual = magnetizeM(numbers)
+                .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                .runIdentity();
+
+        assertThat(actual, contains(iterates(1),
+                                    iterates(2, 2),
+                                    iterates(3, 3, 3),
+                                    iterates(4, 4, 4, 4)));
+    }
+}