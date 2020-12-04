palatable/lambda

Implement FindM
Implement a FindM Fn2 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/FindM.java
@@ -1,0 +1,45 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.HeadM.headM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.FilterM.filterM;
+
+/**
+ * Iterate the elements in an <code>IterateT</code>, applying a predicate to each one, returning the first element that
+ * matches the predicate, wrapped in a {@link Maybe}. If no elements match the predicate, the result is
+ * {@link Maybe#nothing()}. This function short-circuits, and so is safe to use on potentially infinite {@link Iterable}
+ * instances that guarantee to have an eventually matching element.
+ *
+ * @param <A> the IterateT element type
+ * @param <M> the IterateT effect type
+ */
+public class FindM<M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> implements Fn2<Fn1<? super A, Boolean>, IterateT<M, A>, MMA> {
+
+    private static final FindM<?, ?, ?> INSTANCE = new FindM<>();
+
+    private FindM() {
+    }
+
+    @Override
+    public MMA checkedApply(Fn1<? super A, Boolean> predicate, IterateT<M, A> as) throws Throwable {
+        return headM(filterM(predicate, as));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> FindM<M, A, MMA> findM() {
+        return (FindM<M, A, MMA>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> Fn1<IterateT<M, A>, MMA> findM(Fn1<? super A, Boolean> predicate) {
+        return FindM.<M, A, MMA>findM().apply(predicate);
+    }
+
+    public static <M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> MMA findM(Fn1<? super A, Boolean> predicate, IterateT<M, A> as) {
+        return FindM.<M, A, MMA>findM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/FindMTest.java
@@ -1,0 +1,47 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.FindM.findM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GTE.gte;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+
+public class FindMTest {
+    @Test
+    public void findsSomething() {
+        Identity<Maybe<Integer>> actual = findM(gte(4), takeM(10, naturalNumbersM(pureIdentity())));
+        assertThat(actual.runIdentity(), equalTo(just(4)));
+    }
+
+    @Test
+    public void findsNothingWhenEmpty() {
+        Identity<Maybe<Integer>> actual = findM(constantly(true), empty(pureIdentity()));
+        assertThat(actual.runIdentity(), equalTo(nothing()));
+    }
+
+    @Test
+    public void findsNothingWhenNothingMatches() {
+        Identity<Maybe<Integer>> actual = findM(gte(14), takeM(10, naturalNumbersM(pureIdentity())));
+        assertThat(actual.runIdentity(), equalTo(nothing()));
+    }
+
+    @Test
+    public void findsANeedleInAHaystack() {
+        IO<Maybe<Integer>> actual = findM(gte(STACK_EXPLODING_NUMBER), naturalNumbersM(pureIO()));
+        assertThat(actual, yieldsValue(equalTo(just(STACK_EXPLODING_NUMBER))));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/FindM.java
@@ -1,0 +1,45 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.HeadM.headM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.FilterM.filterM;
+
+/**
+ * Iterate the elements in an <code>IterateT</code>, applying a predicate to each one, returning the first element that
+ * matches the predicate, wrapped in a {@link Maybe}. If no elements match the predicate, the result is
+ * {@link Maybe#nothing()}. This function short-circuits, and so is safe to use on potentially infinite {@link Iterable}
+ * instances that guarantee to have an eventually matching element.
+ *
+ * @param <A> the IterateT element type
+ * @param <M> the IterateT effect type
+ */
+public class FindM<M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> implements Fn2<Fn1<? super A, Boolean>, IterateT<M, A>, MMA> {
+
+    private static final FindM<?, ?, ?> INSTANCE = new FindM<>();
+
+    private FindM() {
+    }
+
+    @Override
+    public MMA checkedApply(Fn1<? super A, Boolean> predicate, IterateT<M, A> as) throws Throwable {
+        return headM(FilterM.filterM(predicate, as));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> FindM<M, A, MMA> findM() {
+        return (FindM<M, A, MMA>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> Fn1<IterateT<M, A>, MMA> findM(Fn1<? super A, Boolean> predicate) {
+        return FindM.<M, A, MMA>findM().apply(predicate);
+    }
+
+    public static <M extends MonadRec<?, M>, A, MMA extends MonadRec<Maybe<A>, M>> MMA findM(Fn1<? super A, Boolean> predicate, IterateT<M, A> as) {
+        return FindM.<M, A, MMA>findM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/FindMTest.java
@@ -1,0 +1,47 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.FindM.findM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GTE.gte;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+
+public class FindMTest {
+    @Test
+    public void findsSomething() {
+        Identity<Maybe<Integer>> actual = findM(gte(4), takeM(10, naturalNumbersM(pureIdentity())));
+        assertThat(actual.runIdentity(), equalTo(just(4)));
+    }
+
+    @Test
+    public void findsNothingWhenEmpty() {
+        Identity<Maybe<Integer>> actual = findM(constantly(true), empty(pureIdentity()));
+        assertThat(actual.runIdentity(), equalTo(nothing()));
+    }
+
+    @Test
+    public void findsNothingWhenNothingMatches() {
+        Identity<Maybe<Integer>> actual = findM(gte(14), takeM(10, naturalNumbersM(pureIdentity())));
+        assertThat(actual.runIdentity(), equalTo(nothing()));
+    }
+
+    @Test
+    public void findsANeedleInAHaystack() {
+        IO<Maybe<Integer>> actual = findM(gte(STACK_EXPLODING_NUMBER), naturalNumbersM(pureIO()));
+        assertThat(actual, yieldsValue(equalTo(just(STACK_EXPLODING_NUMBER))));
+    }
+}