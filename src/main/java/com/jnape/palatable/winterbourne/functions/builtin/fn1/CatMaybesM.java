palatable/lambda

Implement CatMaybesM
Implement a CatMaybesM Fn1 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn1/CatMaybesM.java
@@ -1,0 +1,40 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+
+/**
+ * Given an <code>{@link IterateT}&lt;M, {@link Maybe}&lt;A&gt;&gt;</code>, return an
+ * <code>{@link IterateT}&lt;M, A&gt;</code> of only the present values.
+ *
+ * @param <M> the IterateT effect type
+ * @param <A> the {@link Maybe} element type, as well as the resulting {@link IterateT} element type
+ */
+public class CatMaybesM<M extends MonadRec<?, M>, A> implements Fn1<IterateT<M, Maybe<A>>, IterateT<M, A>> {
+
+    private static final CatMaybesM<?, ?> INSTANCE = new CatMaybesM<>();
+
+    private CatMaybesM() {
+    }
+
+    @Override
+    public IterateT<M, A> checkedApply(IterateT<M, Maybe<A>> mas) throws Throwable {
+        return mas.flatMap((Maybe<A> ma) -> ma.match(__ -> empty(Pure.of(mas.<MonadRec<Maybe<Tuple2<Maybe<A>, IterateT<M, Maybe<A>>>>, M>>runIterateT())),
+                                                     mas::pure));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> CatMaybesM<M, A> catMaybesM() {
+        return (CatMaybesM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, A> catMaybesM(IterateT<M, Maybe<A>> mas) {
+        return CatMaybesM.<M, A>catMaybesM().apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn1/CatMaybesMTest.java
@@ -1,0 +1,38 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.CatMaybesM.catMaybesM;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.RepeatM.repeatM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class CatMaybesMTest {
+
+    @Test
+    public void emptyStaysEmpty() {
+        assertThat(catMaybesM(empty(pureIdentity())), isEmpty());
+    }
+
+    @Test
+    public void nothingButNothing() {
+        assertThat(catMaybesM(takeM(4, repeatM(new Identity<>(nothing())))), isEmpty());
+    }
+
+    @Test
+    public void justAFew() {
+        IterateT<Identity<?>, Maybe<Integer>> numbers =
+                maybeT(naturalNumbersM(pureIdentity()).fmap(Maybe::just)).filter(x -> x % 2 == 0).runMaybeT();
+        assertThat(takeM(3, catMaybesM(numbers)), iterates(2, 4, 6));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/CatMaybesM.java
@@ -1,0 +1,40 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+
+/**
+ * Given an <code>{@link IterateT}&lt;M, {@link Maybe}&lt;A&gt;&gt;</code>, return an
+ * <code>{@link IterateT}&lt;M, A&gt;</code> of only the present values.
+ *
+ * @param <M> the IterateT effect type
+ * @param <A> the {@link Maybe} element type, as well as the resulting {@link IterateT} element type
+ */
+public class CatMaybesM<M extends MonadRec<?, M>, A> implements Fn1<IterateT<M, Maybe<A>>, IterateT<M, A>> {
+
+    private static final CatMaybesM<?, ?> INSTANCE = new CatMaybesM<>();
+
+    private CatMaybesM() {
+    }
+
+    @Override
+    public IterateT<M, A> checkedApply(IterateT<M, Maybe<A>> mas) throws Throwable {
+        return mas.flatMap((Maybe<A> ma) -> ma.match(__ -> empty(Pure.of(mas.<MonadRec<Maybe<Tuple2<Maybe<A>, IterateT<M, Maybe<A>>>>, M>>runIterateT())),
+                                                     mas::pure));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> CatMaybesM<M, A> catMaybesM() {
+        return (CatMaybesM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, A> catMaybesM(IterateT<M, Maybe<A>> mas) {
+        return CatMaybesM.<M, A>catMaybesM().apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/CatMaybesMTest.java
@@ -1,0 +1,38 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.CatMaybesM.catMaybesM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.RepeatM.repeatM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class CatMaybesMTest {
+
+    @Test
+    public void emptyStaysEmpty() {
+        assertThat(catMaybesM(empty(pureIdentity())), isEmpty());
+    }
+
+    @Test
+    public void nothingButNothing() {
+        assertThat(catMaybesM(takeM(4, repeatM(new Identity<>(nothing())))), isEmpty());
+    }
+
+    @Test
+    public void justAFew() {
+        IterateT<Identity<?>, Maybe<Integer>> numbers =
+                maybeT(naturalNumbersM(pureIdentity()).fmap(Maybe::just)).filter(x -> x % 2 == 0).runMaybeT();
+        assertThat(takeM(3, catMaybesM(numbers)), iterates(2, 4, 6));
+    }
+}