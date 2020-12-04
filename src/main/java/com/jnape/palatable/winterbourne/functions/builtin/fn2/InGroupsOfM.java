palatable/lambda

Implement InGroupsOfM
Implement an InGroupsOfM Fn2 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/InGroupsOfM.java
@@ -1,0 +1,51 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.CycleM.cycleM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.EchoM.echoM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Eq.eq;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.MagnetizeByM.magnetizeByM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.ZipM.zipM;
+
+/**
+ * Lazily group the <code>IterateT</code> by returning an <code>IterateT</code> of smaller <code>IterateT</code>s of
+ * size <code>k</code>. Note that groups are <em>not</em> padded; that is, if <code>k &gt;= n</code>, where
+ * <code>n</code> is the number of remaining elements, the final <code>IterateT</code> will have only <code>n</code>
+ * elements.
+ *
+ * @param <A> The IterateT element type
+ * @param <M> The IterateT effect type
+ */
+public class InGroupsOfM<M extends MonadRec<?, M>, A> implements Fn2<Integer, IterateT<M, A>, IterateT<M, IterateT<M, A>>> {
+
+    private static final InGroupsOfM<?, ?> INSTANCE = new InGroupsOfM<>();
+
+    private InGroupsOfM() {
+    }
+
+    @Override
+    public IterateT<M, IterateT<M, A>> checkedApply(Integer k, IterateT<M, A> mas) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = mas.runIterateT();
+        return magnetizeByM((t1, t2) -> eq(t1._1(), t2._1()), zipM(echoM(k, cycleM(IterateT.of(unwrapped.pure(true), unwrapped.pure(false)))), mas))
+                .fmap(it -> it.fmap(Tuple2::_2));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> InGroupsOfM<M, A> inGroupsOfM() {
+        return (InGroupsOfM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> Fn1<IterateT<M, A>, IterateT<M, IterateT<M, A>>> inGroupsOfM(Integer k) {
+        return InGroupsOfM.<M, A>inGroupsOfM().apply(k);
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, IterateT<M, A>> inGroupsOfM(Integer k, IterateT<M, A> mas) {
+        return InGroupsOfM.<M, A>inGroupsOfM(k).apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/InGroupsOfMTest.java
@@ -1,0 +1,39 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.InGroupsOfM.inGroupsOfM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class InGroupsOfMTest {
+
+    @Test
+    public void evenlyDividedGroupsOfTwo() {
+        IterateT<Identity<?>, Integer> numbers = takeM(6, naturalNumbersM(pureIdentity()));
+        List<IterateT<Identity<?>, Integer>> actual =
+                inGroupsOfM(2, numbers)
+                        .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                        .runIdentity();
+        assertThat(actual, contains(iterates(1, 2), iterates(3, 4), iterates(5, 6)));
+    }
+
+    @Test
+    public void groupsOfTwoWithLastGroupShort() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        List<IterateT<Identity<?>, Integer>> actual =
+                inGroupsOfM(2, numbers)
+                        .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                        .runIdentity();
+        assertThat(actual, contains(iterates(1, 2), iterates(3, 4), iterates(5)));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/InGroupsOfM.java
@@ -1,0 +1,51 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.CycleM.cycleM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.EchoM.echoM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Eq.eq;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.MagnetizeByM.magnetizeByM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.ZipM.zipM;
+
+/**
+ * Lazily group the <code>IterateT</code> by returning an <code>IterateT</code> of smaller <code>IterateT</code>s of
+ * size <code>k</code>. Note that groups are <em>not</em> padded; that is, if <code>k &gt;= n</code>, where
+ * <code>n</code> is the number of remaining elements, the final <code>IterateT</code> will have only <code>n</code>
+ * elements.
+ *
+ * @param <A> The IterateT element type
+ * @param <M> The IterateT effect type
+ */
+public class InGroupsOfM<M extends MonadRec<?, M>, A> implements Fn2<Integer, IterateT<M, A>, IterateT<M, IterateT<M, A>>> {
+
+    private static final InGroupsOfM<?, ?> INSTANCE = new InGroupsOfM<>();
+
+    private InGroupsOfM() {
+    }
+
+    @Override
+    public IterateT<M, IterateT<M, A>> checkedApply(Integer k, IterateT<M, A> mas) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = mas.runIterateT();
+        return magnetizeByM((t1, t2) -> eq(t1._1(), t2._1()), ZipM.zipM(EchoM.echoM(k, cycleM(IterateT.of(unwrapped.pure(true), unwrapped.pure(false)))), mas))
+                .fmap(it -> it.fmap(Tuple2::_2));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> InGroupsOfM<M, A> inGroupsOfM() {
+        return (InGroupsOfM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> Fn1<IterateT<M, A>, IterateT<M, IterateT<M, A>>> inGroupsOfM(Integer k) {
+        return InGroupsOfM.<M, A>inGroupsOfM().apply(k);
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, IterateT<M, A>> inGroupsOfM(Integer k, IterateT<M, A> mas) {
+        return InGroupsOfM.<M, A>inGroupsOfM(k).apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/InGroupsOfMTest.java
@@ -1,0 +1,39 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.InGroupsOfM.inGroupsOfM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class InGroupsOfMTest {
+
+    @Test
+    public void evenlyDividedGroupsOfTwo() {
+        IterateT<Identity<?>, Integer> numbers = takeM(6, naturalNumbersM(pureIdentity()));
+        List<IterateT<Identity<?>, Integer>> actual =
+                inGroupsOfM(2, numbers)
+                        .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                        .runIdentity();
+        assertThat(actual, contains(iterates(1, 2), iterates(3, 4), iterates(5, 6)));
+    }
+
+    @Test
+    public void groupsOfTwoWithLastGroupShort() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        List<IterateT<Identity<?>, Integer>> actual =
+                inGroupsOfM(2, numbers)
+                        .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                        .runIdentity();
+        assertThat(actual, contains(iterates(1, 2), iterates(3, 4), iterates(5)));
+    }
+}