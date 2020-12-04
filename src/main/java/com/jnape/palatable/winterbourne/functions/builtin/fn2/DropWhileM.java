palatable/lambda

Implement DropWhileM
Implement a DropWhileM Fn2 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/DropWhileM.java
@@ -1,0 +1,46 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.recursion.RecursiveResult;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
+import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.terminate;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+
+public class DropWhileM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final DropWhileM<?, ?> INSTANCE = new DropWhileM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = as.runIterateT();
+        return suspended(
+                () -> unwrapped.trampolineM(mta -> mta
+                        .match(constantly(unwrapped.pure(terminate(nothing()))),
+                               into((A h, IterateT<M, A> t) ->
+                                            predicate.apply(h) ? t.runIterateT().fmap(RecursiveResult::recurse)
+                                                               : unwrapped.pure(terminate(mta))))),
+                Pure.of(unwrapped));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> DropWhileM<A, M> dropWhileM() {
+        return (DropWhileM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> dropWhileM(Fn1<? super A, ? extends Boolean> predicate) {
+        return DropWhileM.<A, M>dropWhileM().apply(predicate);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> dropWhileM(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return DropWhileM.<A, M>dropWhileM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/DropWhileMTest.java
@@ -1,0 +1,58 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.HeadM.headM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.DropWhileM.dropWhileM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LTE.lte;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class DropWhileMTest {
+
+    private static final Fn1<Integer, Integer> MOD_7 = i -> i % 7;
+
+    @Test
+    public void dropEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(dropWhileM(constantly(true), numbers), isEmpty());
+    }
+
+    @Test
+    public void dropAll() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(dropWhileM(lt(100), numbers), isEmpty());
+    }
+
+    @Test
+    public void dropSome() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(dropWhileM(lte(5), numbers), iterates(6, 7, 8, 9, 10));
+    }
+
+    @Test
+    public void dropSomeIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(dropWhileM(lte(5).diMapL(MOD_7), numbers), iterates(6, 7, 8, 9, 10));
+    }
+    @Test
+    public void dropStackSafe() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        Identity<Maybe<Integer>> actual = headM(dropWhileM(lte(STACK_EXPLODING_NUMBER), numbers));
+        assertEquals(just(STACK_EXPLODING_NUMBER + 1), actual.runIdentity());
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/DropWhileM.java
@@ -1,0 +1,46 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.recursion.RecursiveResult;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
+import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.terminate;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+
+public class DropWhileM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final DropWhileM<?, ?> INSTANCE = new DropWhileM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = as.runIterateT();
+        return suspended(
+                () -> unwrapped.trampolineM(mta -> mta
+                        .match(constantly(unwrapped.pure(terminate(nothing()))),
+                               into((A h, IterateT<M, A> t) ->
+                                            predicate.apply(h) ? t.runIterateT().fmap(RecursiveResult::recurse)
+                                                               : unwrapped.pure(terminate(mta))))),
+                Pure.of(unwrapped));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> DropWhileM<A, M> dropWhileM() {
+        return (DropWhileM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> dropWhileM(Fn1<? super A, ? extends Boolean> predicate) {
+        return DropWhileM.<A, M>dropWhileM().apply(predicate);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> dropWhileM(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return DropWhileM.<A, M>dropWhileM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/DropWhileMTest.java
@@ -1,0 +1,58 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.HeadM.headM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.DropWhileM.dropWhileM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LTE.lte;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class DropWhileMTest {
+
+    private static final Fn1<Integer, Integer> MOD_7 = i -> i % 7;
+
+    @Test
+    public void dropEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(dropWhileM(constantly(true), numbers), isEmpty());
+    }
+
+    @Test
+    public void dropAll() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(dropWhileM(lt(100), numbers), isEmpty());
+    }
+
+    @Test
+    public void dropSome() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(dropWhileM(lte(5), numbers), iterates(6, 7, 8, 9, 10));
+    }
+
+    @Test
+    public void dropSomeIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(dropWhileM(lte(5).diMapL(MOD_7), numbers), iterates(6, 7, 8, 9, 10));
+    }
+    @Test
+    public void dropStackSafe() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        Identity<Maybe<Integer>> actual = headM(dropWhileM(lte(STACK_EXPLODING_NUMBER), numbers));
+        assertEquals(just(STACK_EXPLODING_NUMBER + 1), actual.runIdentity());
+    }
+}