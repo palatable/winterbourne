palatable/lambda

Implement TakeM
Implement a TakeM Fn2 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/TakeM.java
@@ -1,0 +1,53 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GT.gt;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
+
+/**
+ * Lazily limit the <code>IterateT</code> to <code>n</code> elements by returning an <code>IterateT</code> that stops
+ * iteration after the <code>nth</code> element, or the last element of the <code>IterateT</code>, whichever comes
+ * first.
+ *
+ * @param <A> The IterateT element type
+ * @param <M> the IterateT effect type
+ * @see TakeWhileM
+ * @see DropM
+ */
+public class TakeM<A, M extends MonadRec<?, M>> implements Fn2<Integer, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final TakeM<?, ?> INSTANCE = new TakeM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Integer n, IterateT<M, A> as) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = as.runIterateT();
+        return suspended(() -> gt(0, n)
+                               ? maybeT(unwrapped)
+                                       .fmap(t -> t.fmap(takeM(n - 1)))
+                                       .runMaybeT()
+                               : unwrapped.pure(nothing()),
+                         Pure.of(unwrapped));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> TakeM<A, M> takeM() {
+        return (TakeM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> takeM(Integer n) {
+        return TakeM.<A, M>takeM().apply(n);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> takeM(Integer n, IterateT<M, A> as) {
+        return TakeM.<A, M>takeM(n).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/TakeMTest.java
@@ -1,0 +1,63 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.of;
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iteratesAll;
+
+public class TakeMTest {
+
+    @Test
+    public void takeEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(takeM(5, numbers), isEmpty());
+    }
+
+    @Test
+    public void takeAllShort() {
+        IterateT<Identity<?>, Integer> numbers = of(new Identity<>(1), new Identity<>(2), new Identity<>(3));
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3)));
+    }
+
+    @Test
+    public void takeAll() {
+        IterateT<Identity<?>, Integer> numbers = of(new Identity<>(1), new Identity<>(2), new Identity<>(3),
+                                                    new Identity<>(4), new Identity<>(5));
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void takeSome() {
+        IterateT<Identity<?>, Integer> numbers = of(new Identity<>(1), new Identity<>(2), new Identity<>(3),
+                                                    new Identity<>(4), new Identity<>(5), new Identity<>(6));
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void takeSomeInfinite() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void takeStackSafe() {
+        IterateT<IO<?>, Integer> numbers = naturalNumbersM(pureIO());
+        assertThat(takeM(STACK_EXPLODING_NUMBER, numbers).fold((a, i) -> io(a + i), io(0)),
+                   yieldsValue(equalTo((STACK_EXPLODING_NUMBER + 1) * (STACK_EXPLODING_NUMBER / 2))));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/TakeM.java
@@ -1,0 +1,53 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GT.gt;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
+
+/**
+ * Lazily limit the <code>IterateT</code> to <code>n</code> elements by returning an <code>IterateT</code> that stops
+ * iteration after the <code>nth</code> element, or the last element of the <code>IterateT</code>, whichever comes
+ * first.
+ *
+ * @param <A> The IterateT element type
+ * @param <M> the IterateT effect type
+ * @see TakeWhileM
+ * @see DropM
+ */
+public class TakeM<A, M extends MonadRec<?, M>> implements Fn2<Integer, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final TakeM<?, ?> INSTANCE = new TakeM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Integer n, IterateT<M, A> as) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = as.runIterateT();
+        return suspended(() -> gt(0, n)
+                               ? maybeT(unwrapped)
+                                       .fmap(t -> t.fmap(takeM(n - 1)))
+                                       .runMaybeT()
+                               : unwrapped.pure(nothing()),
+                         Pure.of(unwrapped));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> TakeM<A, M> takeM() {
+        return (TakeM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> takeM(Integer n) {
+        return TakeM.<A, M>takeM().apply(n);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> takeM(Integer n, IterateT<M, A> as) {
+        return TakeM.<A, M>takeM(n).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/TakeMTest.java
@@ -1,0 +1,63 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.of;
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iteratesAll;
+
+public class TakeMTest {
+
+    @Test
+    public void takeEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(takeM(5, numbers), isEmpty());
+    }
+
+    @Test
+    public void takeAllShort() {
+        IterateT<Identity<?>, Integer> numbers = of(new Identity<>(1), new Identity<>(2), new Identity<>(3));
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3)));
+    }
+
+    @Test
+    public void takeAll() {
+        IterateT<Identity<?>, Integer> numbers = of(new Identity<>(1), new Identity<>(2), new Identity<>(3),
+                                                    new Identity<>(4), new Identity<>(5));
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void takeSome() {
+        IterateT<Identity<?>, Integer> numbers = of(new Identity<>(1), new Identity<>(2), new Identity<>(3),
+                                                    new Identity<>(4), new Identity<>(5), new Identity<>(6));
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void takeSomeInfinite() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        assertThat(takeM(5, numbers), iteratesAll(asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void takeStackSafe() {
+        IterateT<IO<?>, Integer> numbers = naturalNumbersM(pureIO());
+        assertThat(takeM(STACK_EXPLODING_NUMBER, numbers).fold((a, i) -> io(a + i), io(0)),
+                   yieldsValue(equalTo((STACK_EXPLODING_NUMBER + 1) * (STACK_EXPLODING_NUMBER / 2))));
+    }
+}