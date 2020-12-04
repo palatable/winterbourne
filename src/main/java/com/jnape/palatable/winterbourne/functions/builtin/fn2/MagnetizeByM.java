palatable/lambda

Implement MagnetizeByM
Implement a MagnetizeByM Fn2 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/MagnetizeByM.java
@@ -1,0 +1,68 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.adt.hlist.Tuple3;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.recursion.RecursiveResult;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into3.into3;
+import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.recurse;
+import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.terminate;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+
+/**
+ * Given a binary predicate and an <code>{@link IterateT}&lt;M, A&gt;</code>, return an <code>{@link IterateT}&lt;M, {@link
+ * IterateT}&lt;M, A&gt;&gt;</code> of the contiguous groups of elements that match the predicate pairwise.
+ * <p>
+ * See <code>{@link MagnetizeBy}</code> for an example using <code>Iterable</code>
+ *
+ * @param <A> the {@link IterateT} element type
+ * @param <M> the {@link IterateT} effect type
+ */
+public class MagnetizeByM<M extends MonadRec<?, M>, A> implements Fn2<Fn2<? super A, ? super A, Boolean>, IterateT<M, A>, IterateT<M, IterateT<M, A>>> {
+
+    private static final MagnetizeByM<?, ?> INSTANCE = new MagnetizeByM<>();
+
+    private MagnetizeByM() {
+    }
+
+    @Override
+    public IterateT<M, IterateT<M, A>> checkedApply(Fn2<? super A, ? super A, Boolean> predicate, IterateT<M, A> mas) {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrappedA = mas.runIterateT();
+        return suspended(() -> unwrappedA
+                                 .fmap(m -> m.fmap(into((pivot, ys) -> tuple(pivot, mas.pure(pivot), ys))))
+                                 .trampolineM(m -> m.match(
+                                         __ -> unwrappedA.pure(terminate(nothing())),
+                                         into3((pivot, group, ys) -> ys.runIterateT().<RecursiveResult<Maybe<Tuple3<A, IterateT<M, A>, IterateT<M, A>>>, Maybe<Tuple2<IterateT<M, A>, IterateT<M, A>>>>>fmap(m2 -> m2.match(
+                                                 __ -> terminate(just(tuple(group, empty(Pure.of(unwrappedA))))),
+                                                 into((y, tail) ->
+                                                              predicate.apply(pivot, y)
+                                                              ? recurse(just(tuple(y, group.concat(mas.pure(y)), tail)))
+                                                              : terminate(just(tuple(group, mas.pure(y).concat(tail))))))))))
+                                 .fmap(m -> m.fmap(t -> t.fmap(magnetizeByM(predicate)))),
+                         Pure.of(unwrappedA));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> MagnetizeByM<M, A> magnetizeByM() {
+        return (MagnetizeByM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> Fn1<IterateT<M, A>, IterateT<M, IterateT<M, A>>> magnetizeByM(Fn2<? super A, ? super A, Boolean> predicate) {
+        return MagnetizeByM.<M, A>magnetizeByM().apply(predicate);
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, IterateT<M, A>> magnetizeByM(Fn2<? super A, ? super A, Boolean> predicate, IterateT<M, A> mas) {
+        return MagnetizeByM.<M, A>magnetizeByM(predicate).apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/MagnetizeByMTest.java
@@ -1,0 +1,86 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+import testsupport.matchers.IterableMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.CycleM.cycleM;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Drop.drop;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GTE.gte;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.MagnetizeByM.magnetizeByM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Map.map;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.NthM.nthM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Take.take;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functions.builtin.fn3.FoldLeft.foldLeft;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.singleton;
+import static java.util.Arrays.asList;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class MagnetizeByMTest {
+    @Test
+    public void magnetizesEmpty() {
+        assertThat(magnetizeByM(GTE.<Integer>gte(), empty(pureIdentity())), isEmpty());
+    }
+
+    @Test
+    public void magnetizesSingleton() {
+        List<IterateT<Identity<?>, Integer>> actual = magnetizeByM(gte(), singleton(new Identity<>(1)))
+                .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                .runIdentity();
+        assertThat(actual, contains(iterates(1)));
+    }
+
+    @Test
+    public void magnetizesElementsInSeveralGroups() {
+        List<IterateT<Identity<?>, Integer>> actual = magnetizeByM(gte(), fromIterable(pureIdentity(), asList(1, 2, 3, 2, 2, 3, 2, 1)))
+                .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                .runIdentity();
+        assertThat(actual, contains(iterates(1, 2, 3),
+                                    iterates(2, 2, 3),
+                                    iterates(2),
+                                    iterates(1)));
+    }
+
+    @Test
+    public void magnetizesLargeGroups() {
+        IterateT<Identity<?>, Integer> numbers = cycleM(takeM(10_000, naturalNumbersM(pureIdentity())));
+        Identity<Maybe<IterateT<Identity<?>, Integer>>> maybeIdentity = nthM(3, magnetizeByM(gte(), numbers)).coerce();
+        List<Integer> thirdGroup = maybeIdentity
+                .runIdentity()
+                .orElseThrow(AssertionError::new)
+                .<List<Integer>, Identity<List<Integer>>>toCollection(ArrayList::new)
+                .runIdentity();
+        assertThat(thirdGroup, hasSize(10_000));
+        assertThat(take(3, thirdGroup), IterableMatcher.iterates(1, 2, 3));
+        assertThat(drop(9_997, thirdGroup), IterableMatcher.iterates(9_998, 9_999, 10_000));
+    }
+
+    @Test
+    public void magnetizesLotsOfSmallGroups() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        Identity<Maybe<IterateT<Identity<?>, Integer>>> group = nthM(STACK_EXPLODING_NUMBER, magnetizeByM(lt(), numbers)).coerce();
+        IterateT<Identity<?>, Integer> actual = group.runIdentity().orElseThrow(AssertionError::new);
+        assertThat(actual, iterates(STACK_EXPLODING_NUMBER));
+    }
+
+    private static <M extends MonadRec<?, M>, A> IterateT<M, A> fromIterable(Pure<M> pureM, Iterable<A> as) {
+        return foldLeft(IterateT::snoc, empty(pureM), map(pureM::<A, MonadRec<A, M>>apply, as));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/MagnetizeByM.java
@@ -1,0 +1,69 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.adt.hlist.Tuple3;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.builtin.fn2.MagnetizeBy;
+import com.jnape.palatable.lambda.functions.recursion.RecursiveResult;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into3.into3;
+import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.recurse;
+import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.terminate;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
+
+/**
+ * Given a binary predicate and an <code>{@link IterateT}&lt;M, A&gt;</code>, return an <code>{@link IterateT}&lt;M, {@link
+ * IterateT}&lt;M, A&gt;&gt;</code> of the contiguous groups of elements that match the predicate pairwise.
+ * <p>
+ * See <code>{@link MagnetizeBy}</code> for an example using <code>Iterable</code>
+ *
+ * @param <A> the {@link IterateT} element type
+ * @param <M> the {@link IterateT} effect type
+ */
+public class MagnetizeByM<M extends MonadRec<?, M>, A> implements Fn2<Fn2<? super A, ? super A, Boolean>, IterateT<M, A>, IterateT<M, IterateT<M, A>>> {
+
+    private static final MagnetizeByM<?, ?> INSTANCE = new MagnetizeByM<>();
+
+    private MagnetizeByM() {
+    }
+
+    @Override
+    public IterateT<M, IterateT<M, A>> checkedApply(Fn2<? super A, ? super A, Boolean> predicate, IterateT<M, A> mas) {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrappedA = mas.runIterateT();
+        return suspended(() -> unwrappedA
+                                 .fmap(m -> m.fmap(into((pivot, ys) -> tuple(pivot, mas.pure(pivot), ys))))
+                                 .trampolineM(m -> m.match(
+                                         __ -> unwrappedA.pure(terminate(nothing())),
+                                         into3((pivot, group, ys) -> ys.runIterateT().<RecursiveResult<Maybe<Tuple3<A, IterateT<M, A>, IterateT<M, A>>>, Maybe<Tuple2<IterateT<M, A>, IterateT<M, A>>>>>fmap(m2 -> m2.match(
+                                                 __ -> terminate(just(tuple(group, empty(Pure.of(unwrappedA))))),
+                                                 into((y, tail) ->
+                                                              predicate.apply(pivot, y)
+                                                              ? recurse(just(tuple(y, group.concat(mas.pure(y)), tail)))
+                                                              : terminate(just(tuple(group, mas.pure(y).concat(tail))))))))))
+                                 .fmap(m -> m.fmap(t -> t.fmap(magnetizeByM(predicate)))),
+                         Pure.of(unwrappedA));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> MagnetizeByM<M, A> magnetizeByM() {
+        return (MagnetizeByM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> Fn1<IterateT<M, A>, IterateT<M, IterateT<M, A>>> magnetizeByM(Fn2<? super A, ? super A, Boolean> predicate) {
+        return MagnetizeByM.<M, A>magnetizeByM().apply(predicate);
+    }
+
+    public static <M extends MonadRec<?, M>, A> IterateT<M, IterateT<M, A>> magnetizeByM(Fn2<? super A, ? super A, Boolean> predicate, IterateT<M, A> mas) {
+        return MagnetizeByM.<M, A>magnetizeByM(predicate).apply(mas);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/MagnetizeByMTest.java
@@ -1,0 +1,87 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.functions.builtin.fn2.GTE;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+import testsupport.matchers.IterableMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.CycleM.cycleM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Drop.drop;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GTE.gte;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.MagnetizeByM.magnetizeByM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Map.map;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.NthM.nthM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Take.take;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functions.builtin.fn3.FoldLeft.foldLeft;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.singleton;
+import static java.util.Arrays.asList;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class MagnetizeByMTest {
+    @Test
+    public void magnetizesEmpty() {
+        assertThat(magnetizeByM(GTE.<Integer>gte(), empty(pureIdentity())), isEmpty());
+    }
+
+    @Test
+    public void magnetizesSingleton() {
+        List<IterateT<Identity<?>, Integer>> actual = magnetizeByM(gte(), singleton(new Identity<>(1)))
+                .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                .runIdentity();
+        assertThat(actual, contains(iterates(1)));
+    }
+
+    @Test
+    public void magnetizesElementsInSeveralGroups() {
+        List<IterateT<Identity<?>, Integer>> actual = magnetizeByM(gte(), fromIterable(pureIdentity(), asList(1, 2, 3, 2, 2, 3, 2, 1)))
+                .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
+                .runIdentity();
+        assertThat(actual, contains(iterates(1, 2, 3),
+                                    iterates(2, 2, 3),
+                                    iterates(2),
+                                    iterates(1)));
+    }
+
+    @Test
+    public void magnetizesLargeGroups() {
+        IterateT<Identity<?>, Integer> numbers = cycleM(takeM(10_000, naturalNumbersM(pureIdentity())));
+        Identity<Maybe<IterateT<Identity<?>, Integer>>> maybeIdentity = nthM(3, magnetizeByM(gte(), numbers)).coerce();
+        List<Integer> thirdGroup = maybeIdentity
+                .runIdentity()
+                .orElseThrow(AssertionError::new)
+                .<List<Integer>, Identity<List<Integer>>>toCollection(ArrayList::new)
+                .runIdentity();
+        assertThat(thirdGroup, hasSize(10_000));
+        assertThat(take(3, thirdGroup), IterableMatcher.iterates(1, 2, 3));
+        assertThat(drop(9_997, thirdGroup), IterableMatcher.iterates(9_998, 9_999, 10_000));
+    }
+
+    @Test
+    public void magnetizesLotsOfSmallGroups() {
+        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
+        Identity<Maybe<IterateT<Identity<?>, Integer>>> group = nthM(STACK_EXPLODING_NUMBER, magnetizeByM(lt(), numbers)).coerce();
+        IterateT<Identity<?>, Integer> actual = group.runIdentity().orElseThrow(AssertionError::new);
+        assertThat(actual, iterates(STACK_EXPLODING_NUMBER));
+    }
+
+    private static <M extends MonadRec<?, M>, A> IterateT<M, A> fromIterable(Pure<M> pureM, Iterable<A> as) {
+        return foldLeft(IterateT::snoc, empty(pureM), map(pureM::<A, MonadRec<A, M>>apply, as));
+    }
+}