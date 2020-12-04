palatable/lambda

Implement FilterM
Implement a FilterM Fn2 for IterateT

src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/FilterM.java
@@ -1,0 +1,48 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn4.IfThenElse.ifThenElse;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.*;
+
+/**
+ * Lazily apply a predicate to each element in an <code>IterateT</code>, returning an <code>IterateT</code> of just the
+ * elements for which the predicate evaluated to <code>true</code>.
+ *
+ * @param <A> A type contravariant to the input IterateT element type
+ * @param <M> the IterateT effect type
+ * @see TakeWhileM
+ * @see DropWhileM
+ */
+public class FilterM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final FilterM<?, ?> INSTANCE = new FilterM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = as.runIterateT();
+        return iterateT(unwrapped)
+                .flatMap(ifThenElse(predicate,
+                                    a -> singleton(unwrapped.pure(a)),
+                                    __ -> empty(Pure.of(unwrapped))));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> FilterM<A, M> filterM() {
+        return (FilterM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> filterM(Fn1<? super A, ? extends Boolean> predicate) {
+        return FilterM.<A, M>filterM().apply(predicate);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> filterM(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return FilterM.<A, M>filterM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/FilterMTest.java
@@ -1,0 +1,66 @@
+package com.jnape.palatable.lambda.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.FilterM.filterM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GT.gt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.semigroup.builtin.Max.max;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class FilterMTest {
+
+    @Test
+    public void filterEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(filterM(constantly(true), numbers), isEmpty());
+    }
+
+    @Test
+    public void filterNothing() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(constantly(true), numbers), iterates(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void filterAll() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(constantly(false), numbers), isEmpty());
+    }
+
+    @Test
+    public void filterSome() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(gt(3), numbers), iterates(4, 5));
+        assertThat(filterM(lt(3), numbers), iterates(1, 2));
+    }
+
+    @Test
+    public void filterSomeIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(i -> i % 2 == 0, numbers), iterates(2, 4, 6, 8, 10));
+        assertThat(filterM(i -> i % 2 == 1, numbers), iterates(1, 3, 5, 7, 9));
+    }
+
+    @Test
+    public void filterStackSafe() {
+        IterateT<IO<?>, Integer> numbers = takeM(STACK_EXPLODING_NUMBER, naturalNumbersM(pureIO()));
+        assertThat(filterM(constantly(true), numbers).fold((a, i) -> io(max(a, i)), io(0)),
+                   yieldsValue(equalTo(STACK_EXPLODING_NUMBER)));
+    }
+}

palatable/winterbourne

src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/FilterM.java
@@ -1,0 +1,48 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn4.IfThenElse.ifThenElse;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.*;
+
+/**
+ * Lazily apply a predicate to each element in an <code>IterateT</code>, returning an <code>IterateT</code> of just the
+ * elements for which the predicate evaluated to <code>true</code>.
+ *
+ * @param <A> A type contravariant to the input IterateT element type
+ * @param <M> the IterateT effect type
+ * @see TakeWhileM
+ * @see DropWhileM
+ */
+public class FilterM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
+
+    private static final FilterM<?, ?> INSTANCE = new FilterM<>();
+
+    @Override
+    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) throws Throwable {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> unwrapped = as.runIterateT();
+        return iterateT(unwrapped)
+                .flatMap(ifThenElse(predicate,
+                                    a -> singleton(unwrapped.pure(a)),
+                                    __ -> empty(Pure.of(unwrapped))));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>> FilterM<A, M> filterM() {
+        return (FilterM<A, M>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> filterM(Fn1<? super A, ? extends Boolean> predicate) {
+        return FilterM.<A, M>filterM().apply(predicate);
+    }
+
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> filterM(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+        return FilterM.<A, M>filterM(predicate).apply(as);
+    }
+}

src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/FilterMTest.java
@@ -1,0 +1,66 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn2;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import com.jnape.palatable.lambda.io.IO;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.FilterM.filterM;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.GT.gt;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.pureIO;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.semigroup.builtin.Max.max;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+import static testsupport.matchers.IterateTMatcher.isEmpty;
+import static testsupport.matchers.IterateTMatcher.iterates;
+
+public class FilterMTest {
+
+    @Test
+    public void filterEmpty() {
+        IterateT<Identity<?>, Integer> numbers = empty(pureIdentity());
+        assertThat(filterM(constantly(true), numbers), isEmpty());
+    }
+
+    @Test
+    public void filterNothing() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(constantly(true), numbers), iterates(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    public void filterAll() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(constantly(false), numbers), isEmpty());
+    }
+
+    @Test
+    public void filterSome() {
+        IterateT<Identity<?>, Integer> numbers = takeM(5, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(gt(3), numbers), iterates(4, 5));
+        assertThat(filterM(lt(3), numbers), iterates(1, 2));
+    }
+
+    @Test
+    public void filterSomeIntermittent() {
+        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()));
+        assertThat(filterM(i -> i % 2 == 0, numbers), iterates(2, 4, 6, 8, 10));
+        assertThat(filterM(i -> i % 2 == 1, numbers), iterates(1, 3, 5, 7, 9));
+    }
+
+    @Test
+    public void filterStackSafe() {
+        IterateT<IO<?>, Integer> numbers = takeM(STACK_EXPLODING_NUMBER, naturalNumbersM(pureIO()));
+        assertThat(filterM(constantly(true), numbers).fold((a, i) -> io(max(a, i)), io(0)),
+                   yieldsValue(equalTo(STACK_EXPLODING_NUMBER)));
+    }
+}