a77cc0ebc5d6394997ad696a333ef299b0baf58b
jnape <jnape09@gmail.com>
2020-12-07 16:11:29 -0600

    MagnetizeM cleanup

--- src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/MagnetizeMTest.java
+++ src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/MagnetizeMTest.java
@@ -7,27 +7,26 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.jnape.palatable.winterbourne.functions.builtin.fn1.MagnetizeM.magnetizeM;
-import static com.jnape.palatable.winterbourne.functions.builtin.fn1.RepeatM.repeatM;
-import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
 import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
-import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
-import static org.junit.Assert.*;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.MagnetizeM.magnetizeM;
 import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.ReplicateM.replicateM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
 import static testsupport.matchers.IterateTMatcher.iterates;
 
 public class MagnetizeMTest {
 
     @Test
     public void groupsLikeElements() {
-        IterateT<Identity<?>, Integer> numbers = takeM(10, naturalNumbersM(pureIdentity()).flatMap(n -> takeM(n, repeatM(new Identity<>(n)))));
-        List<IterateT<Identity<?>, Integer>> actual = magnetizeM(numbers)
-                .<List<IterateT<Identity<?>, Integer>>, Identity<List<IterateT<Identity<?>, Integer>>>>toCollection(ArrayList::new)
-                .runIdentity();
-
-        assertThat(actual, contains(iterates(1),
-                                    iterates(2, 2),
-                                    iterates(3, 3, 3),
-                                    iterates(4, 4, 4, 4)));
+        Identity<List<IterateT<Identity<?>, Integer>>> actual = magnetizeM(takeM(10, naturalNumbersM(pureIdentity())
+                .flatMap(n -> replicateM(n, new Identity<>(n)))))
+                .toCollection(ArrayList::new);
+        assertThat(actual.runIdentity(),
+                   contains(iterates(1),
+                            iterates(2, 2),
+                            iterates(3, 3, 3),
+                            iterates(4, 4, 4, 4)));
     }
 }
