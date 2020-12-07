4f6ee4d000496c80fdaf942ee5163772d54c9bb4
jnape <jnape09@gmail.com>
2020-12-07 15:58:49 -0600

    HeadM cleanup

--- src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/HeadM.java
+++ src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/HeadM.java
@@ -12,18 +12,17 @@ import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT
  * Retrieve the head element of an {@link IterateT}, wrapped in an {@link Maybe}. If the {@link IterateT} is empty, the
  * result is {@link Maybe#nothing()}.
  *
- * @param <A> the IterateT element type
- * @param <M> the IterateT effect type
+ * @param <A> the {@link IterateT} element type
+ * @param <M> the {@link IterateT} effect type
  */
-public class HeadM<A, M extends MonadRec<?, M>, MMA extends MonadRec<Maybe<A>, M>> implements Fn1<IterateT<M, A>, MMA> {
+public final class HeadM<A, M extends MonadRec<?, M>, MMA extends MonadRec<Maybe<A>, M>>
+        implements Fn1<IterateT<M, A>, MMA> {
 
     private static final HeadM<?, ?, ?> INSTANCE = new HeadM<>();
 
     @Override
     public MMA checkedApply(IterateT<M, A> as) {
-        return maybeT(as.runIterateT())
-                .fmap(Tuple2::_1)
-                .runMaybeT();
+        return maybeT(as.runIterateT()).fmap(Tuple2::_1).runMaybeT();
     }
 
     @SuppressWarnings("unchecked")
--- src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/HeadMTest.java
+++ src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/HeadMTest.java
@@ -1,28 +1,25 @@
 package com.jnape.palatable.winterbourne.functions.builtin.fn1;
 
-import com.jnape.palatable.lambda.adt.Maybe;
 import com.jnape.palatable.lambda.functor.builtin.Identity;
 import org.junit.Test;
 
 import static com.jnape.palatable.lambda.adt.Maybe.just;
 import static com.jnape.palatable.lambda.adt.Maybe.nothing;
-import static com.jnape.palatable.winterbourne.functions.builtin.fn1.HeadM.headM;
 import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
 import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
-import static org.junit.Assert.assertEquals;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.HeadM.headM;
 import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static org.junit.Assert.assertEquals;
 
 public class HeadMTest {
 
     @Test
     public void headEmpty() {
-        Identity<Maybe<Integer>> mma = headM(empty(pureIdentity()));
-        assertEquals(mma.runIdentity(), nothing());
+        assertEquals(new Identity<>(nothing()), headM(empty(pureIdentity())));
     }
 
     @Test
-    public void headInfinite() {
-        Identity<Maybe<Integer>> mma = headM(naturalNumbersM(pureIdentity()));
-        assertEquals(mma.runIdentity(), just(1));
+    public void headNonEmpty() {
+        assertEquals(new Identity<>(just(1)), headM(naturalNumbersM(pureIdentity())));
     }
 }
