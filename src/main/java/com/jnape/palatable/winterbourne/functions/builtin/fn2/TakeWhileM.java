ddcf4a9f786b63fc5e63210da28d63fbc4089216
jnape <jnape09@gmail.com>
2020-12-07 14:29:26 -0600

    TakeWhileM is now stack-safe

--- src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/TakeWhileM.java
+++ src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn2/TakeWhileM.java
@@ -1,12 +1,15 @@
 package com.jnape.palatable.winterbourne.functions.builtin.fn2;
 
+import com.jnape.palatable.lambda.adt.Maybe;
 import com.jnape.palatable.lambda.adt.hlist.Tuple2;
 import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.lambda.functions.Fn2;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
 import com.jnape.palatable.lambda.monad.MonadRec;
 import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
 
 import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.iterateT;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.suspended;
 import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
 
 /**
@@ -19,15 +22,18 @@ import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT
  * @see FilterM
  * @see DropWhileM
  */
-public class TakeWhileM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
+public class TakeWhileM<A, M extends MonadRec<?, M>> implements
+        Fn2<Fn1<? super A, ? extends Boolean>, IterateT<M, A>, IterateT<M, A>> {
 
     private static final TakeWhileM<?, ?> INSTANCE = new TakeWhileM<>();
 
     @Override
-    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
-        return iterateT(maybeT(as.runIterateT())
+    public IterateT<M, A> checkedApply(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> mas) {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> head = mas.runIterateT();
+        return iterateT(maybeT(head)
                                 .filter(predicate.diMapL(Tuple2::_1))
-                                .fmap(t -> t.fmap(takeWhileM(predicate)))
+                                .fmap(t -> t.fmap(as -> suspended(() -> takeWhileM(predicate, as).runIterateT(),
+                                                                  Pure.of(head))))
                                 .runMaybeT());
     }
 
@@ -36,11 +42,13 @@ public class TakeWhileM<A, M extends MonadRec<?, M>> implements Fn2<Fn1<? super 
         return (TakeWhileM<A, M>) INSTANCE;
     }
 
-    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> takeWhileM(Fn1<? super A, ? extends Boolean> predicate) {
+    public static <A, M extends MonadRec<?, M>> Fn1<IterateT<M, A>, IterateT<M, A>> takeWhileM(
+            Fn1<? super A, ? extends Boolean> predicate) {
         return TakeWhileM.<A, M>takeWhileM().apply(predicate);
     }
 
-    public static <A, M extends MonadRec<?, M>> IterateT<M, A> takeWhileM(Fn1<? super A, ? extends Boolean> predicate, IterateT<M, A> as) {
+    public static <A, M extends MonadRec<?, M>> IterateT<M, A> takeWhileM(Fn1<? super A, ? extends Boolean> predicate,
+                                                                          IterateT<M, A> as) {
         return TakeWhileM.<A, M>takeWhileM(predicate).apply(as);
     }
 }
