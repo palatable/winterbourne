1a3a9fd2643d08d9aeaa52f78ab6557c4919b45e
jnape <jnape09@gmail.com>
2020-12-07 16:17:27 -0600

    NaturalNumbersM cleanup

--- src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/NaturalNumbersM.java
+++ src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/NaturalNumbersM.java
@@ -7,9 +7,13 @@ import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
 
 import static com.jnape.palatable.lambda.adt.Maybe.just;
 import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
-import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.unfold;
 
-public class NaturalNumbersM<M extends MonadRec<?, M>> implements Fn1<Pure<M>, IterateT<M, Integer>> {
+/**
+ * Given a {@link Pure}, produce an {@link IterateT} of the positive natural numbers.
+ *
+ * @param <M> the effect type
+ */
+public final class NaturalNumbersM<M extends MonadRec<?, M>> implements Fn1<Pure<M>, IterateT<M, Integer>> {
 
     private static final NaturalNumbersM<?> INSTANCE = new NaturalNumbersM<>();
 
@@ -18,7 +22,7 @@ public class NaturalNumbersM<M extends MonadRec<?, M>> implements Fn1<Pure<M>, I
 
     @Override
     public IterateT<M, Integer> checkedApply(Pure<M> pureM) {
-        return unfold(i -> pureM.apply(just(tuple(i, i + 1))), pureM.<Integer, MonadRec<Integer, M>>apply(1));
+        return IterateT.<M, Integer, Integer>unfold(i -> pureM.apply(just(tuple(i, i + 1))), pureM.apply(1));
     }
 
     @SuppressWarnings("unchecked")
--- src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/NaturalNumbersMTest.java
+++ src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/NaturalNumbersMTest.java
@@ -1,61 +1,34 @@
 package com.jnape.palatable.winterbourne.functions.builtin.fn1;
 
-import com.jnape.palatable.lambda.functor.builtin.Identity;
-import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
 import org.junit.Test;
 
-import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
-import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.LT.lt;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.LTE.lte;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.Snoc.snoc;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.Tupler2.tupler;
-import static com.jnape.palatable.lambda.functions.builtin.fn4.IfThenElse.ifThenElse;
-import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.recurse;
-import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.terminate;
 import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
-import static java.util.Collections.emptyList;
-import static org.junit.Assert.*;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.DropM.dropM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static org.junit.Assert.assertThat;
 import static testsupport.Constants.STACK_EXPLODING_NUMBER;
-import static testsupport.matchers.IterableMatcher.iterates;
+import static testsupport.matchers.IterateTMatcher.iterates;
 
 public class NaturalNumbersMTest {
+
     @Test
     public void producesTheNats() {
-        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
-        Iterable<Integer> sample = numbers
-                .<Iterable<Integer>, Identity<Iterable<Integer>>>foldCut(
-                        (acc, n) -> new Identity<>(lte(10, n)
-                                                   ? recurse(snoc(n, acc))
-                                                   : terminate(acc)),
-                        new Identity<>(emptyList()))
-                .runIdentity();
-        assertThat(sample, iterates(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        assertThat(takeM(10, naturalNumbersM(pureIdentity())), iterates(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
     }
 
     @Test
     public void producesNatsForever() {
-        IterateT<Identity<?>, Integer> numbers = naturalNumbersM(pureIdentity());
-        Iterable<Integer> sample = numbers
-                .<Iterable<Integer>, Identity<Iterable<Integer>>>foldCut(
-                        (acc, n) -> new Identity<>(lt(STACK_EXPLODING_NUMBER, n)
-                                                   ? recurse(acc)
-                                                   : lte(STACK_EXPLODING_NUMBER + 10, n)
-                                                     ? recurse(snoc(n, acc))
-                                                     : terminate(acc)),
-                        new Identity<>(emptyList()))
-                .runIdentity();
-        assertThat(sample, iterates(STACK_EXPLODING_NUMBER,
-                                    STACK_EXPLODING_NUMBER + 1,
-                                    STACK_EXPLODING_NUMBER + 2,
-                                    STACK_EXPLODING_NUMBER + 3,
-                                    STACK_EXPLODING_NUMBER + 4,
-                                    STACK_EXPLODING_NUMBER + 5,
-                                    STACK_EXPLODING_NUMBER + 6,
-                                    STACK_EXPLODING_NUMBER + 7,
-                                    STACK_EXPLODING_NUMBER + 8,
-                                    STACK_EXPLODING_NUMBER + 9,
-                                    STACK_EXPLODING_NUMBER + 10));
+        assertThat(dropM(STACK_EXPLODING_NUMBER, takeM(STACK_EXPLODING_NUMBER + 10, naturalNumbersM(pureIdentity()))),
+                   iterates(STACK_EXPLODING_NUMBER + 1,
+                            STACK_EXPLODING_NUMBER + 2,
+                            STACK_EXPLODING_NUMBER + 3,
+                            STACK_EXPLODING_NUMBER + 4,
+                            STACK_EXPLODING_NUMBER + 5,
+                            STACK_EXPLODING_NUMBER + 6,
+                            STACK_EXPLODING_NUMBER + 7,
+                            STACK_EXPLODING_NUMBER + 8,
+                            STACK_EXPLODING_NUMBER + 9,
+                            STACK_EXPLODING_NUMBER + 10));
     }
 }
