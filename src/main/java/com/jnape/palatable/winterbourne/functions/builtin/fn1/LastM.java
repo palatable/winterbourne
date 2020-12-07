c7c8f83e22e8dc0c0ccb7943df25a4a61ddbe049
jnape <jnape09@gmail.com>
2020-12-07 15:12:38 -0600

    Adding LastM for extracting the last effect of an IterateT

---
+++ src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/LastM.java
@@ -0,0 +1,43 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.recursion.RecursiveResult;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.monad.transformer.builtin.IterateT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
+import static com.jnape.palatable.lambda.functions.recursion.RecursiveResult.terminate;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.MaybeT.maybeT;
+
+/**
+ * Retrieve the last element of an {@link IterateT}, wrapped in a {@link Maybe}. If the {@link IterateT} is empty, the
+ * result is {@link Maybe#nothing()}.
+ *
+ * @param <A> the IterateT element type
+ * @param <M> the IterateT effect type
+ */
+public class LastM<A, M extends MonadRec<?, M>, MMA extends MonadRec<Maybe<A>, M>> implements Fn1<IterateT<M, A>, MMA> {
+
+    private static final LastM<?, ?, ?> INSTANCE = new LastM<>();
+
+    @Override
+    public MMA checkedApply(IterateT<M, A> mas) {
+        MonadRec<Maybe<Tuple2<A, IterateT<M, A>>>, M> headM = mas.runIterateT();
+        return maybeT(headM).trampolineM(into((a, as) -> maybeT(as.runIterateT())
+                .<RecursiveResult<Tuple2<A, IterateT<M, A>>, A>>fmap(RecursiveResult::recurse)
+                .or(maybeT(headM.pure(just(terminate(a)))))))
+                .runMaybeT();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A, M extends MonadRec<?, M>, MMA extends MonadRec<Maybe<A>, M>> LastM<A, M, MMA> lastM() {
+        return (LastM<A, M, MMA>) INSTANCE;
+    }
+
+    public static <A, M extends MonadRec<?, M>, MMA extends MonadRec<Maybe<A>, M>> MMA lastM(IterateT<M, A> as) {
+        return LastM.<A, M, MMA>lastM().apply(as);
+    }
+}
---
+++ src/test/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/LastMTest.java
@@ -0,0 +1,30 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functor.builtin.Identity;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.functor.builtin.Identity.pureIdentity;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.empty;
+import static com.jnape.palatable.lambda.monad.transformer.builtin.IterateT.singleton;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.LastM.lastM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn1.NaturalNumbersM.naturalNumbersM;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.TakeM.takeM;
+import static org.junit.Assert.assertEquals;
+import static testsupport.Constants.STACK_EXPLODING_NUMBER;
+
+public class LastMTest {
+
+    @Test
+    public void lastEmpty() {
+        assertEquals(new Identity<>(nothing()), lastM(empty(pureIdentity())));
+    }
+
+    @Test
+    public void lastNonEmpty() {
+        assertEquals(new Identity<>(just(0)), lastM(singleton(new Identity<>(0))));
+        assertEquals(new Identity<>(just(STACK_EXPLODING_NUMBER)),
+                     lastM(takeM(STACK_EXPLODING_NUMBER, naturalNumbersM(pureIdentity()))));
+    }
+}
