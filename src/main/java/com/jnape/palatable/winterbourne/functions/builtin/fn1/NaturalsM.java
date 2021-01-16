0d754fc88618468f6a58287e00d7b88eb29a38e1
Nate Riffe <inkblot@movealong.org>
2021-01-16 12:03:24 -0600

    Implement NaturalsM

---
+++ src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/NaturalsM.java
@@ -0,0 +1,42 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.specialized.Pure;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.shoki.api.Natural;
+import com.jnape.palatable.winterbourne.StreamT;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.$.$;
+import static com.jnape.palatable.shoki.api.Natural.zero;
+import static com.jnape.palatable.winterbourne.StreamT.unfold;
+
+/**
+ * Given a {@link Pure}, produce a <code>{@link StreamT}&lt;M, Natural&gt;</code> starting with {@link Natural#zero()}
+ * and increasing by {@link Natural#one()} on each emission.
+ *
+ * @param <M> the effect type
+ */
+public class NaturalsM<M extends MonadRec<?, M>> implements Fn1<Pure<M>, StreamT<M, Natural>> {
+
+    private static final NaturalsM<?> INSTANCE = new NaturalsM<>();
+
+    private NaturalsM() {
+    }
+
+    @Override
+    public StreamT<M, Natural> checkedApply(Pure<M> pureM) {
+        return unfold(n -> pureM.apply(just(tuple(just(n), n.inc()))),
+                      pureM.<Natural, MonadRec<Natural, M>>apply(zero()));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>> NaturalsM<M> naturalsM() {
+        return (NaturalsM<M>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>> StreamT<M, Natural> naturalsM(Pure<M> pureM) {
+        return $(naturalsM(), pureM);
+    }
+}
