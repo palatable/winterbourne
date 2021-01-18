248ceb2219abdfad452c4249bd62a28da824560b
Nate Riffe <inkblot@movealong.org>
2021-01-18 00:08:45 -0600

    Implement MagnetizeM

---
+++ src/main/java/com/jnape/palatable/winterbourne/functions/builtin/fn1/MagnetizeM.java
@@ -0,0 +1,38 @@
+package com.jnape.palatable.winterbourne.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+import com.jnape.palatable.lambda.functions.builtin.fn1.Magnetize;
+import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.winterbourne.StreamT;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn2.$.$;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Eq.eq;
+import static com.jnape.palatable.winterbourne.functions.builtin.fn2.MagnetizeByM.magnetizeByM;
+
+/**
+ * {@link Magnetize} a <code>{@link StreamT}&lt;M, A&gt;</code> using value equality as the magnetizing function.
+ *
+ * @param <M> the {@link StreamT} effect type
+ * @param <A> the {@link StreamT} element type
+ */
+public final class MagnetizeM<M extends MonadRec<?, M>, A> implements Fn1<StreamT<M, A>, StreamT<M, StreamT<M, A>>> {
+
+    private static final MagnetizeM<?, ?> INSTANCE = new MagnetizeM<>();
+
+    private MagnetizeM() {
+    }
+
+    @Override
+    public StreamT<M, StreamT<M, A>> checkedApply(StreamT<M, A> mas) throws Throwable {
+        return magnetizeByM(eq(), mas);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <M extends MonadRec<?, M>, A> MagnetizeM<M, A> magnetizeM() {
+        return (MagnetizeM<M, A>) INSTANCE;
+    }
+
+    public static <M extends MonadRec<?, M>, A> StreamT<M, StreamT<M, A>> magnetizeM(StreamT<M, A> as) {
+        return $(magnetizeM(), as);
+    }
+}
